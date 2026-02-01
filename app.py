8b9ca999e6615ca06f78f557991acf046bb5641f
sivagopal <sivagopalakrishna1234@gmail.com>
2026-02-01 17:45:09 +0530

    Trigger CI/CD

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+cat <<EOF > app.py
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GKE CI/CD!"
+    return "Hello from gopala gopal Cloud Build CI/CD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+EOF
