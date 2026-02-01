ec1417f1031c5e7e3f6851529695030acfe5d1ef
sivagopal <sivagopalakrishna1234@gmail.com>
2026-02-01 18:03:26 +0530

    Trigger CI/CD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GKE CI/CD!"
+    return "Hello from GKE CI/CD mr gopinadh developed own he is leader!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
